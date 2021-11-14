d35f17c60a14a664dcb7ef4a064918ba20c829eb
Andres Felipe Gonzalez <andresfelipegonzalez@hotmail.com>
2021-11-14 01:53:48 -0500

    [DEVELOP] installed react swiper

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,6 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, Fragment } from 'react'
+import {Swiper} from 'swiper'
+import {SwiperSlide} from 'swiper/react'
 
 const Home = ({characters}) => {
   const [chars, setChars] = useState()
@@ -34,24 +36,7 @@ const Home = ({characters}) => {
       {selectedChar ? 
         <CharacterBio character={selectedChar} goBack={goBack} />
         : 
-        <div className="card-container flex flex-wrap justify-center md:justify-start gap-3 md:w-2/3">
-          {(chars && chars.length > 0) ?
-            chars.map((char, index) => char.image.url &&
-              <div
-                key={index}
-                className={`card relative w-28 h-28 text-white rounded-md border-solid border-2 border-white overflow-hidden m-0 cursor-pointer`}
-                onClick={() => selectCard(char)}
-                onMouseEnter={() => setCurrentChar(char.image.url)}
-                onMouseLeave={() => setCurrentChar("")}
-              >
-                <img src={char.image.url} alt={char.name} className={`${!char.selected && 'opacity-50'}`} />
-                {!char.selected && <span className="block absolute top-0 right-2">NEW</span>}
-              </div>
-            )
-            :
-            undefined
-          }
-        </div>
+        <CharacterGallery chars={chars} />
       }
       <div className={`char-image w-full mt-3 md:mb-0 md:w-1/3 flex justify-center md:ml-3`}>
         <img src={currentChar} alt="" />
@@ -83,5 +68,71 @@ const CharacterBio = ({character, goBack}) => {
         ))}
       </ul>
     </div>
+  )
+}
+
+const CharacterGallery = ({chars}) => {
+  const [ viewportWidth, setViewportWidth ] = useState(window.innerWidth)
+
+  useEffect(() => {
+    window.addEventListener("resize", handleWindowResize)
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [window.innerWidth])
+
+  const handleWindowResize = () => {
+    setViewportWidth( window.innerWidth )
+  }
+
+  return (
+    <Fragment>
+      {viewportWidth <= 768 ? 
+        <Swiper 
+          slidesPerView={'auto'} 
+          spaceBetween={30} 
+          pagination={{
+            "clickable": true
+          }} 
+          className="mySwiper"
+        >
+          {(chars && chars.length > 0) ?
+            chars.map((char, index) => char.image.url &&
+              <SwiperSlide key={index}>
+                <div
+                  className={`card relative w-28 h-28 text-white rounded-md border-solid border-2 border-white overflow-hidden m-0 cursor-pointer`}
+                  onClick={() => selectCard(char)}
+                  onMouseEnter={() => setCurrentChar(char.image.url)}
+                  onMouseLeave={() => setCurrentChar("")}
+                >
+                  <img src={char.image.url} alt={char.name} className={`${!char.selected && 'opacity-50'}`} />
+                  {!char.selected && <span className="block absolute top-0 right-2">NEW</span>}
+                </div>
+              </SwiperSlide>
+            )
+            :
+            undefined
+          }
+        </Swiper>
+        : 
+        <div className="card-container flex flex-wrap justify-center md:justify-start gap-3 md:w-2/3">
+          {(chars && chars.length > 0) ?
+            chars.map((char, index) => char.image.url &&
+              <div
+                key={index}
+                className={`card relative w-28 h-28 text-white rounded-md border-solid border-2 border-white overflow-hidden m-0 cursor-pointer`}
+                onClick={() => selectCard(char)}
+                onMouseEnter={() => setCurrentChar(char.image.url)}
+                onMouseLeave={() => setCurrentChar("")}
+              >
+                <img src={char.image.url} alt={char.name} className={`${!char.selected && 'opacity-50'}`} />
+                {!char.selected && <span className="block absolute top-0 right-2">NEW</span>}
+              </div>
+            )
+            :
+            undefined
+          }
+        </div>
+      }
+      
+    </Fragment>
   )
 }
